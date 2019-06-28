d206eb18e9b12b13b0a15fa422dc4fbf99a1b115
XATTpy <zoomerrr@mail.ru>
2019-06-28 19:28:42 +0300

    add resize function

--- image_resize.py
+++ image_resize.py
@@ -50,9 +50,11 @@ def get_new_sizes(args, ratio, sign, width, height):
     elif args.width and not args.height:
         new_width = args.width
         new_height = new_width * ratio if sign else new_width / ratio
+        new_height = round(new_height)
     elif not args.width and args.height:
         new_height = args.height
         new_width = new_height / ratio if sign else new_height * ratio
+        new_width = round(new_width)
     else:
         new_width, new_height = width, height
     return new_width, new_height
@@ -63,18 +65,34 @@ def get_scale(args):
     return scale
 
 
+def get_new_sizes_by_scale(scale, width, height):
+    new_width = round(width*scale)
+    new_height = round(height*scale)
+    return new_width, new_height
+
+
+def get_new_img(img, new_width, new_height):
+    new_img = img.resize((new_width, new_height))
+    return new_img
+
+
 if __name__ == '__main__':
     args = get_args()
+    img_path = get_imgpath(args)
+    img = load_img(img_path)
+    width, height = get_sizes(img)
 
     scale = get_scale(args)
     if scale and (args.width or args.height):
         quit('If a scale is specified, then the width and height cannot be specified.')
     elif scale >= 1:
         quit('Scale must be less than 1')
+    elif scale:
+        new_width, new_height = get_new_sizes_by_scale(scale, width, height)
+        print(new_width, new_height)
+    else:
+        ratio, sign = get_ratio(width, height)
+        new_width, new_height = get_new_sizes(args, ratio, sign, width, height)
 
-    img_path = get_imgpath(args)
-    img = load_img(img_path)
-    width, height = get_sizes(img)
-    ratio, sign = get_ratio(width, height)
-    new_width, new_height = get_new_sizes(args, ratio, sign, width, height)
-    img.show()
+    new_img = get_new_img(img, new_width, new_height)
+    new_img.show()
